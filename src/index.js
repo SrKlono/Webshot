ed881b2949dea8dfc987788b39782efd11a2d8d3
SrKlono <danilogamasilvanb@gmail.com>
2025-11-30 12:57:10 -0300

    Rate limit added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { getDesktopWebshot, getMobileWebshot } from "./lib/playwright.js";
+import rateLimit from "express-rate-limit";
 
 dotenv.config();
 
@@ -11,11 +12,20 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 30,
+	standardHeaders: "draft-8",
+	legacyHeaders: false,
+	ipv6Subnet: 56,
+});
+
 const router = express.Router();
 
 router.get("/", getDesktopWebshot);
 router.get("/mobile/", getMobileWebshot);
 
+app.use(limiter);
 app.use("/", router);
 
 app.listen(PORT, () => {
