4a4bd399331030e1f5fca87880d0563619dadef7
SrKlono <danilogamasilvanb@gmail.com>
2025-12-01 14:50:35 -0300

    Timeout time increased

--- src/lib/playwright.js
+++ src/lib/playwright.js
@@ -5,7 +5,7 @@ export async function getDesktopWebshot(req, res) {
 	try {
 		getWebshot(devices["Desktop Chrome"], req, res);
 	} catch (error) {
-		console.log("Error in playwright", error);
+		console.log("Error in getDesktopWebshot", error);
 	}
 }
 
@@ -13,7 +13,7 @@ export async function getMobileWebshot(req, res) {
 	try {
 		getWebshot(devices["iPhone 14 Pro Max"], req, res);
 	} catch (error) {
-		console.log("Error in playwright", error);
+		console.log("Error in getMobileWebshot", error);
 	}
 }
 
@@ -35,11 +35,11 @@ async function getWebshot(device, req, res) {
 	const page = await context.newPage();
 
 	try {
-		await page.goto(url, { waitUntil: "load", timeout: 30000 });
+		await page.goto(url, { waitUntil: "load", timeout: 60000 });
 	} catch (error) {
-		console.log("Error in goto website getWebShot lib", error);
+		console.log("Error in getWebShot", error);
 		if (error.name === "TimeoutError") {
-			res.status(408).json({ message: "Timeout 30s exceeded." });
+			res.status(408).json({ message: "Timeout of 1 minute exceeded." });
 			return;
 		}
 		res.status(400).json({ message: "Error loading website" });
